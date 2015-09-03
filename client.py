cd08b8105f4462d680f605672166be579917d0a1
doyler <dev@doyler.net>
2015-09-03 15:41:28 +0200

    Adding basic chat functionality

--- client.py
+++ client.py
@@ -8,13 +8,67 @@ Client application for the PyChat client
 
 import socket
 
+# chat_client.py
+
+import sys
+import socket
+import select
+
+def send_msg(sock):
+    while True:
+        data = sys.stdin.readline()
+        sock.sendto(data, target)
+
+def recv_msg(sock):
+    while True:
+        data, addr = sock.recvfrom(1024)
+        sys.stdout.write(data)
+
 # create the socket
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# get hostname
-host = socket.gethostname()
+# server information
+host = "127.0.0.1"
 port = 9998
 
+# set a timeout of 2 seconds for the socket
+clientSocket.settimeout(2)
+
+# connect to the server
+try:
+    clientSocket.connect((host, port))
+except:
+    print "Error connecting to the server."
+    sys.exit()
+    
+print "Connected to the remote host, you can now start sending messages."
+
+sys.stdout.write('[Me] '); sys.stdout.flush()
+
+while True:
+    socketList = [sys.stdin, clientSocket]
+    
+    # get the list of currently readable sockets from select()
+    readSockets, writeSockets, errorSockets = select.select(socketList, [], [])
+    
+    for sock in readSockets:
+        # incoming message from the server
+        if sock == clientSocket:
+            data = sock.recv(4096)
+            if not data:
+                print "\nDisconnected from the chat server."
+                sys.exit()
+            else:
+                
+                sys.stdout.write(data)
+                sys.stdout.write('[Me] '); sys.stdout.flush() 
+        # outgoing message to the server
+        else:
+            message = sys.stdin.readline()
+            clientSocket.send(msg)
+            sys.stdout.write('[Me] '); sys.stdout.flush() 
+
+"""
 # connect to the server
 clientSocket.connect((host, port))
 
@@ -24,3 +78,4 @@ msg = clientSocket.recv(1024)
 clientSocket.close()
 
 print "The message from the server was: %s" % msg.decode("ascii")
+"""
--- server.py
+++ server.py
@@ -7,26 +7,65 @@ Server application for the PyChat client
 """
 
 import socket
-import time
+import select
+#import time
+
+def broadcastMessage(sock, message):
+    # do not send the message to the server or the client that sent the message
+    for socket in CONNECTION_LIST:
+        if socket != serverSocket and socket != sock:
+            try:
+                socket.send(message)
+            except:
+                # unable to connect to the socket for some reason
+                socket.close()
+                CONNECTION_LIST.remove(socket)
+
+# a list of all current, active connections to the server
+CONNECTION_LIST = []
+
+# buffer to receive messages into
+RECV_BUFFER = 4096
 
 # create the socket
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#serverSocket = socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # get hostname
-host = socket.gethostname()
+# host = socket.gethostname()
 port = 9998
 
-# bind to the port
-serverSocket.bind((host, port))
+# bind to all local IP addresses on the specific port
+serverSocket.bind(("0.0.0.0", port))
 
-# queue up to 5 requests
-serverSocket.listen(5)
+# queue up to 10 requests at a time
+serverSocket.listen(10)
+
+# add the server to the list of active connections
+CONNECTION_LIST.append(serverSocket)
 
 while True:
-    # establish the connection
-    clientSocket, clientAddr = serverSocket.accept()
-    
-    print "Got a connection from %s" % str(clientAddr)
-    currentTime = time.ctime(time.time()) + "\r\n"
-    clientSocket.send(currentTime.encode("ascii"))
-    clientSocket.close()
+    # get the list of currently readable sockets from select()
+    readSockets, writeSockets, errorSockets = select.select(CONNECTION_LIST, [], [])
+ 
+    for sock in readSockets:
+        # new connections
+        if sock == serverSocket:
+            clientSocket, clientAddr = serverSocket.accept()
+            CONNECTION_LIST.append(clientSocket)
+            print "Client (%s, %s) connected!" % clientAddr
+            broadcastMessage(clientSocket, "[%s:%s] has entered the room.\n" % clientAddr)
+        # incoming message from a client
+        else:
+            try:
+                data = sock.recv(RECV_BUFFER)
+                if data:
+                    broadcastMessage(sock, "\r" + "<" + str(sock.getpeername()) + "> " + data)
+            except:
+                broadcastMessage(sock, "Client (%s, %s) is offline" % clientAddr)
+                print "Client (%s, %s) is offline" % clientAddr
+                sock.close()
+                CONNECTION_LIST.remove(sock)
+                continue
+     
+serverSocket.close()
